533a85fe0a00656dce292d957fe662a92487dc46
Ravi Tiwari <tiwarirabi3@gmail.com>
2020-06-23 13:45:25 +0545

    Initial Commit.

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Home.scss';
+
+export default props => {
+    return(
+        <div className="HomePage">
+            React PWA Starter
+        </div>
+    );
+}
